031f861fa9897bd289bae557e90121639aba15f7
eunjiom <norgg021@gmail.com>
2026-04-03 11:45:36 +0900

    feat: 2-8 QueryDSL 변경

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -10,13 +10,13 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoRepositoryCustom {
 
     // 검색용 메서드 변경(""" > 문자열 시작)
     @Query(" SELECT t FROM Todo t LEFT JOIN FETCH t.user " +
             "WHERE (:weather IS NULL OR t.weather = :weather)" +
             "AND (:startDate IS NULL OR t.modifiedAt >= :startDate)" +
-            "AND (:endDate IS NULL OR t.modifiedAt <= : endDate)" +
+            "AND (:endDate IS NULL OR t.modifiedAt <= :endDate)" +
             "ORDER BY t.modifiedAt DESC ")
     Page<Todo> searchTodos(
             @Param("weather") String weather,
@@ -24,9 +24,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             @Param("endDate") LocalDateTime endDate,
             Pageable pageable
             );
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
